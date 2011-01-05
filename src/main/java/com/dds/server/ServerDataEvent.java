661187bcd679c79dac81aec20e59c5d6cbda657f
Utkarsh Sengar <utkarsh2012@gmail.com>
2011-01-05 14:13:00 +0530

    changed sockets to NIO

---
+++ src/main/java/com/dds/server/ServerDataEvent.java
@@ -0,0 +1,15 @@
+package com.dds.server;
+
+import java.nio.channels.SocketChannel;
+
+class ServerDataEvent {
+	public DDSServer server;
+	public SocketChannel socket;
+	public byte[] data;
+
+	public ServerDataEvent(DDSServer server, SocketChannel socket, byte[] data) {
+		this.server = server;
+		this.socket = socket;
+		this.data = data;
+	}
+}
