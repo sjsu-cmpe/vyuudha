b17b15c722ceee7abe1a077642dbb9918bc9f3b4
Ravi Dontharaju <ravi.dontharaju@gmail.com>
2011-02-05 04:14:52 -0800

    Nearly done with MongoDB

--- src/main/java/com/dds/storage/mongodb/MongoDB.java
+++ src/main/java/com/dds/storage/mongodb/MongoDB.java
@@ -4,26 +4,34 @@
 package com.dds.storage.mongodb;
 
 import java.net.UnknownHostException;
-import java.util.Set;
+import java.util.List;
 
+import com.dds.storage.DBInterface;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.Mongo;
 import com.mongodb.MongoException;
 
 /**
  * @author ravid
- * 
+ *
  */
-public class MongoDB {
+public class MongoDB implements DBInterface {
 
 	Mongo m = null;
 	DB db = null;
-
+	DBCollection coll = null;
+	
 	private String dbName = null;
 	private String collection = null;
+	
+	public MongoDB() {
+		setDbName("newDB");
+		setCollection("newCollection");		
+	}
 	
 	/**
 	 * @param dbName the dbName to set
@@ -39,53 +47,125 @@ public class MongoDB {
 		this.collection = collection;
 	}
 	
+	/** 
+	 * Create connection to MongoDB
+	 * 
+	 * @see com.dds.storage.DBInterface#createConnection()
+	 */
 	public void createConnection()
 	{
 		try {
 			m = new Mongo();
+			db = m.getDB( dbName);
+			coll = db.getCollection(collection);
 		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (MongoException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-		db = m.getDB( dbName);
 	}
 	
-	public void show()
+	/** 
+	 * Insert a key and value
+	 * 
+	 * @see com.dds.storage.DBInterface#put(java.lang.String, java.lang.String)
+	 */
+	public void put(String key, String value)
 	{
-		Set<String> colls = db.getCollectionNames();
-
-		for (String s : colls) {
-		    System.out.println(s);
-		}
-	}
-	
-	public void insert(String key, String value)
-	{
-		DBCollection coll = db.getCollection(collection);
         BasicDBObject doc = new BasicDBObject();
 
         doc.put("key", key);
         doc.put("value", value);
 
-        coll.insert(doc);
+        if (!contains(key)) {
+        	coll.insert(doc);
+        }
+        else {
+        	//TODO: Do something somday :)
+        }
 	}
 	
-	public void remove(String key)
+	/**
+	 * Delete a particular key
+	 * 
+	 * @see com.dds.storage.DBInterface#delete(java.lang.String)
+	 */
+	public void delete(String key)
 	{
-		DBCollection coll = db.getCollection(collection);
-		DBObject myDoc = coll.findOne();
-		coll.remove(myDoc);
-		System.out.println("Removed");
+		BasicDBObject lookUp = new BasicDBObject();
+		lookUp.put("key", key);
+		
+        coll.remove(lookUp);
 	}
 	
-	public void get(String key)
+	/**
+	 * Used to retrieve a value for the associated key
+	 *  
+	 * @see com.dds.storage.DBInterface#get(java.lang.String)
+	 */
+	public String get(String key)
 	{
-		DBCollection coll = db.getCollection(collection);
-		DBObject myDoc = coll.findOne();
-		System.out.println(myDoc);
+		BasicDBObject lookUp = new BasicDBObject();
+		lookUp.put("key", key);
+		
+		DBCursor cursor = coll.find(lookUp);
+		List<DBObject> list = cursor.toArray();
+		
+		String value = null;
+		for (DBObject obj : list) {
+			value = obj.get("value").toString();
+		}
+		return value;
+	}
+	
+	/**
+	 * Display all the keys in the Collection
+	 */
+	public void showAll()
+	{
+        DBCursor cur = coll.find();
+
+        while(cur.hasNext()) {
+            System.out.println(cur.next());
+        }
+	}
+	
+	/**
+	 * Delete all keys by dropping the collection
+	 */
+	public void deleteAll()
+	{
+        coll.drop();
+	}
+	
+	/**
+	 * Return true if key is present else false
+	 * 
+	 * @see com.dds.storage.DBInterface#contains(java.lang.String)
+	 */
+	public boolean contains(String key) {
+		BasicDBObject lookUp = new BasicDBObject();
+		lookUp.put("key", key);
+		
+		DBCursor cursor = coll.find(lookUp);
+		if (cursor.count() == 0) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+	
+	/** 
+	 * Close the connections, setting all to none
+	 * No in-built close function for MongoDB
+	 * 
+	 * @see com.dds.storage.DBInterface#closeConnection()
+	 */
+	public void closeConnection() {
+		
+		m = null;
+		db = null;
+		coll  = null;		
 	}
 }
