97193dbbe339faaf622deae14e49dce178a3e1f0
Ravi Dontharaju <ravi.dontharaju@gmail.com>
2011-02-07 01:32:41 -0800

    Minor changes to MongoDB

--- src/main/java/com/dds/storage/AccessMongoDB.java
+++ src/main/java/com/dds/storage/AccessMongoDB.java
@@ -3,6 +3,8 @@
  */
 package com.dds.storage;
 
+import java.net.UnknownHostException;
+
 import com.dds.storage.mongodb.MongoDB;
 
 /**
@@ -18,21 +20,28 @@ public class AccessMongoDB {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		MongoDB mdb = new MongoDB();
-		mdb.createConnection();
+		MongoDB mdb;
+		try {
+			mdb = new MongoDB();
 
-		mdb.put("key1", "value1");
-		mdb.put("key2", "value2");
-		mdb.put("key1", "value3");
-		
-		if (mdb.contains("key1"))
-			System.out.println("Present!");
-		else
-			System.out.println("Not present!");
-		//System.out.println(mdb.get("key2"));
-		//mdb.showAll();
-		//mdb.delete("key1");
-		//mdb.deleteAll();
-		mdb.closeConnection();
+			mdb.createConnection();
+
+			mdb.put("key1", "value1");
+			mdb.put("key2", "value2");
+			mdb.put("key1", "value3");
+
+			if (mdb.contains("key1"))
+				System.out.println("Present!");
+			else
+				System.out.println("Not present!");
+			// System.out.println(mdb.get("key2"));
+			// mdb.showAll();
+			// mdb.delete("key1");
+			// mdb.deleteAll();
+			mdb.closeConnection();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
--- src/main/java/com/dds/storage/mongodb/MongoDB.java
+++ src/main/java/com/dds/storage/mongodb/MongoDB.java
@@ -6,6 +6,8 @@ package com.dds.storage.mongodb;
 import java.net.UnknownHostException;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 import com.dds.storage.DBInterface;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
@@ -17,127 +19,125 @@ import com.mongodb.MongoException;
 
 /**
  * @author ravid
- *
+ * 
  */
-public class MongoDB implements DBInterface {
+public class MongoDB extends Mongo implements DBInterface {
 
-	Mongo m = null;
+	//Mongo m = null;
 	DB db = null;
 	DBCollection coll = null;
-	
+	Logger logger = Logger.getLogger(MongoDB.class);
+
 	private String dbName = null;
 	private String collection = null;
-	
-	public MongoDB() {
+
+	public MongoDB() throws UnknownHostException {
 		setDbName("newDB");
-		setCollection("newCollection");		
+		setCollection("newCollection");
 	}
-	
+
 	/**
-	 * @param dbName the dbName to set
+	 * @param dbName
+	 *            the dbName to set
 	 */
 	private void setDbName(String dbName) {
 		this.dbName = dbName;
 	}
 
 	/**
-	 * @param collection the collection to set
+	 * @param collection
+	 *            the collection to set
 	 */
 	private void setCollection(String collection) {
 		this.collection = collection;
 	}
-	
-	/** 
+
+	/**
 	 * Create connection to MongoDB
 	 * 
 	 * @see com.dds.storage.DBInterface#createConnection()
 	 */
-	public void createConnection()
-	{
+	public void createConnection() {
 		try {
-			m = new Mongo();
-			db = m.getDB( dbName);
+			//m = new Mongo();
+			db = this.getDB(dbName);
 			coll = db.getCollection(collection);
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
+//		} catch (UnknownHostException e) {
+//			e.printStackTrace();
 		} catch (MongoException e) {
 			e.printStackTrace();
 		}
+		logger.info("MongoDB connection created");
 	}
-	
-	/** 
+
+	/**
 	 * Insert a key and value
 	 * 
 	 * @see com.dds.storage.DBInterface#put(java.lang.String, java.lang.String)
 	 */
-	public void put(String key, String value)
-	{
-        BasicDBObject doc = new BasicDBObject();
+	public void put(String key, String value) {
+		BasicDBObject doc = new BasicDBObject();
 
-        doc.put("key", key);
-        doc.put("value", value);
+		doc.put("key", key);
+		doc.put("value", value);
 
-        if (!contains(key)) {
-        	coll.insert(doc);
-        }
-        else {
-        	//TODO: Do something somday :)
-        }
+		if (!contains(key)) {
+			coll.insert(doc);
+		} else {
+			delete(key);
+			coll.insert(doc);
+		}
 	}
-	
+
 	/**
 	 * Delete a particular key
 	 * 
 	 * @see com.dds.storage.DBInterface#delete(java.lang.String)
 	 */
-	public void delete(String key)
-	{
+	public void delete(String key) {
 		BasicDBObject lookUp = new BasicDBObject();
 		lookUp.put("key", key);
-		
-        coll.remove(lookUp);
+
+		coll.remove(lookUp);
 	}
-	
+
 	/**
 	 * Used to retrieve a value for the associated key
-	 *  
+	 * 
 	 * @see com.dds.storage.DBInterface#get(java.lang.String)
 	 */
-	public String get(String key)
-	{
+	public String get(String key) {
 		BasicDBObject lookUp = new BasicDBObject();
 		lookUp.put("key", key);
-		
+
 		DBCursor cursor = coll.find(lookUp);
 		List<DBObject> list = cursor.toArray();
-		
+
 		String value = null;
 		for (DBObject obj : list) {
 			value = obj.get("value").toString();
 		}
 		return value;
 	}
-	
+
 	/**
 	 * Display all the keys in the Collection
 	 */
-	public void showAll()
-	{
-        DBCursor cur = coll.find();
+	public void showAll() {
+		DBCursor cur = coll.find();
 
-        while(cur.hasNext()) {
-            System.out.println(cur.next());
-        }
+		while (cur.hasNext()) {
+			System.out.println(cur.next());
+		}
 	}
-	
+
 	/**
 	 * Delete all keys by dropping the collection
 	 */
-	public void deleteAll()
-	{
-        coll.drop();
+	public void deleteAll() {
+		coll.drop();
 	}
-	
+
 	/**
 	 * Return true if key is present else false
 	 * 
@@ -146,26 +146,24 @@ public class MongoDB implements DBInterface {
 	public boolean contains(String key) {
 		BasicDBObject lookUp = new BasicDBObject();
 		lookUp.put("key", key);
-		
+
 		DBCursor cursor = coll.find(lookUp);
 		if (cursor.count() == 0) {
 			return false;
-		}
-		else {
+		} else {
 			return true;
 		}
 	}
-	
-	/** 
-	 * Close the connections, setting all to none
-	 * No in-built close function for MongoDB
+
+	/**
+	 * Close the connections, setting all to none No in-built close function for
+	 * MongoDB
 	 * 
 	 * @see com.dds.storage.DBInterface#closeConnection()
 	 */
 	public void closeConnection() {
-		
-		m = null;
+
 		db = null;
-		coll  = null;		
+		coll = null;
 	}
 }
