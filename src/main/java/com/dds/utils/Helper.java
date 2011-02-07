d400a373734dc96ed49c4f3664314c4cb4fc77c7
Ravi Dontharaju <ravi.dontharaju@gmail.com>
2011-02-07 01:33:35 -0800

    Added a getObject function for un-marshaling from byte array to object

--- src/main/java/com/dds/utils/Helper.java
+++ src/main/java/com/dds/utils/Helper.java
@@ -1,17 +1,54 @@
 package com.dds.utils;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class Helper {
-	public byte[] getBytes(Object obj) throws java.io.IOException {
+	public byte[] getBytes(Object obj) {
+
 		ByteArrayOutputStream bos = new ByteArrayOutputStream();
-		ObjectOutputStream oos = new ObjectOutputStream(bos);
-		oos.writeObject(obj);
-		oos.flush();
-		oos.close();
-		bos.close();
+		try {
+			ObjectOutputStream oos = new ObjectOutputStream(bos);
+			oos.writeObject(obj);
+			oos.flush();
+			oos.close();
+			bos.close();
+		} catch (IOException ioex) {
+			ioex.printStackTrace();
+		}
 		byte[] data = bos.toByteArray();
 		return data;
+	}
+	
+	/**
+	 * Takes a byte array as input and returns an Object.
+	 * Need to add a type cast in the calling function.
+	 * 
+	 * eg. (String) getObject(byteArray);
+	 * 
+	 * @param bytes
+	 * @return Generic Object
+	 */
+	public Object getObject(byte[] bytes) {
+
+        Object data = null;
+    	ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+    	ObjectInputStream ois = null;
+
+    	try {
+			ois = new ObjectInputStream(bis);
+			data = ois.readObject();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}   
+        
+        return data;
 	}
 }
