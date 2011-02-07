99436f2b536064303a5db6a71b13f2c6533f7e36
Ravi Dontharaju <ravi.dontharaju@gmail.com>
2011-02-07 01:32:22 -0800

    BDB changes have been made.
    basic, get/put/delete/contain work fine now.
    
    Issues: BDB cannot extend Database without a second layer of
    abstraction.
    Issues discussed on github

--- src/main/java/com/dds/storage/AccessBDB.java
+++ src/main/java/com/dds/storage/AccessBDB.java
@@ -1,23 +1,39 @@
 package com.dds.storage;
 
+import java.io.File;
+
 import com.dds.storage.bdb.BDB;
 
 public class AccessBDB {
 
 	public static void main(String[] args)
 	{
-		BDB dbdEnv = new BDB();
-
+		BDB dbdEnv = null;
 		try {
-			
-			dbdEnv.createConnection();
+			StringBuilder path = new StringBuilder(new java.io.File(".").getCanonicalPath());
+			path.append("/store/bdb/");
 
-		    
+			File bdbPath = new File(path.toString());
+			if (!bdbPath.exists()) {
+			 	bdbPath.mkdirs();
+		 	}
+			
+			BDB.setBdbPath(path.toString());
+			dbdEnv = new BDB();
+			dbdEnv.createConnection();
+			
+			//dbdEnv.put("key1", "value4");
+			//dbdEnv.put("key2", "value2");
+			System.out.println(dbdEnv.contains("key4"));
+			System.out.println(dbdEnv.get("key2"));
+					    
 		} catch(Exception dbe) {
 			System.out.println(dbe.toString());
 		}
 		finally {
-			dbdEnv.closeConnection();
+			if (dbdEnv != null) {
+				dbdEnv.closeConnection();
+			}
 		} 
 	}
 }
--- src/main/java/com/dds/storage/bdb/BDB.java
+++ src/main/java/com/dds/storage/bdb/BDB.java
@@ -1,21 +1,31 @@
 package com.dds.storage.bdb;
 
 import java.io.File;
+import java.util.List;
+
 import org.apache.log4j.Logger;
 
 import com.dds.storage.DBInterface;
+import com.sleepycat.je.Cursor;
 import com.sleepycat.je.Database;
 import com.sleepycat.je.DatabaseConfig;
+import com.sleepycat.je.DatabaseEntry;
 import com.sleepycat.je.DatabaseException;
 import com.sleepycat.je.Environment;
 import com.sleepycat.je.EnvironmentConfig;
+import com.sleepycat.je.LockMode;
+import com.sleepycat.je.OperationStatus;
+import com.dds.utils.Helper;
+import com.google.common.collect.Lists;
 
 public class BDB implements DBInterface{
 
-	private Environment myEnv;
+	private static String bdbPath;
+	private static Environment myEnv;
 	private Database vendorDb;
-	private Database database;
+	
 	Logger logger = Logger.getLogger(BDB.class);
+	private final Helper helper;
 	
 	File envHome;
 	boolean readOnly;
@@ -29,11 +39,19 @@ public class BDB implements DBInterface{
 
 	public BDB() {
 		//create folder in <project base>/store/part1
-		setEnvHome(new File("store/part1/"));
+		setBdbPath(bdbPath);
+		setEnvHome(new File(bdbPath));
 		setReadOnly(false);
-		database = getVendorDB();
+		helper = new Helper();
 	}
 	
+	/**
+	 * @param bdbPath the bdbPath to set
+	 */
+	public static void setBdbPath(String _bdbPath) {
+		bdbPath = _bdbPath;
+	}
+
 	/**
 	 * @param envHome the envHome to set
 	 */
@@ -56,6 +74,7 @@ public class BDB implements DBInterface{
 			// Now create and open our databases.
 			logger.info("Create and open database");
 			vendorDb = myEnv.openDatabase(null, "VendorDB", myDbConfig);
+			
 		} catch (Exception ex) {
 			logger.info("Error in setup: " + ex.toString());
 		}
@@ -71,26 +90,67 @@ public class BDB implements DBInterface{
 	}
 
 	public String get(String key) {
-		//database.get(null, key, null, null);
-		return null;
+		
+		DatabaseEntry entryKey = new DatabaseEntry(helper.getBytes(key));
+		
+		Cursor cursor = null;
+        DatabaseEntry entryValue = new DatabaseEntry();
+        List<Object> results = Lists.newArrayList();
+        LockMode lockMode = LockMode.DEFAULT;
+		try {
+			cursor = getVendorDB().openCursor(null, null);
+	        for(OperationStatus status = cursor.getSearchKey(entryKey, entryValue, lockMode); 
+	        status == OperationStatus.SUCCESS; status = cursor.getNextDup(entryKey, entryValue, lockMode)) {
+	        	results.add(helper.getObject(entryValue.getData()));
+	        }
+	        cursor.close();
+		} catch (DatabaseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		Object retObj = null;
+		for (Object obj : results)	{
+			retObj = obj;
+		}
+		
+		return (String) retObj;
 	}
 
 	public void put(String key, String value) {
-		//database.put(null, key, value);
+		DatabaseEntry entryKey = new DatabaseEntry(helper.getBytes(key));
+		DatabaseEntry entryValue = new DatabaseEntry(helper.getBytes(value));
 		
+		try {
+			getVendorDB().put(null, entryKey, entryValue);
+		} catch (DatabaseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		logger.info("Key " + key + " inserted!");
 	}
 
 	public void delete(String key) {
-		// TODO Auto-generated method stub
+		DatabaseEntry entryKey = new DatabaseEntry(helper.getBytes(key));
 		
+		try {
+			OperationStatus status = getVendorDB().delete(null, entryKey);
+			if (status == OperationStatus.SUCCESS) {
+				logger.info("Key " + key + " deleted");
+			} else {
+				logger.info("Key " + key + " not found");
+			}			
+		} catch (DatabaseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
 	}
 
 	public void closeConnection() {
 		logger.info("Closing database");
 		if (myEnv != null) {
-			try {
-				vendorDb.close();
-				myEnv.close();
+			try {		
+				getVendorDB().close();
+				getEnvironment().close();
 			} catch (DatabaseException dbe) {
 				logger.info("Error in closing database: " + dbe.toString());
 				System.exit(-1);
@@ -99,7 +159,23 @@ public class BDB implements DBInterface{
 	}
 
 	public boolean contains(String key) {
-		// TODO Auto-generated method stub
-		return false;
+		DatabaseEntry entryKey = new DatabaseEntry(helper.getBytes(key));
+		
+		boolean keyFound = false;
+		Cursor cursor = null;
+        DatabaseEntry entryValue = new DatabaseEntry();
+
+		try {
+			cursor = getVendorDB().openCursor(null, null);
+	        OperationStatus status = cursor.getSearchKey(entryKey, entryValue, LockMode.DEFAULT); 
+	        if (status == OperationStatus.SUCCESS) {
+	        	keyFound = true;
+	        }
+	        cursor.close();
+		} catch (DatabaseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		return keyFound;
 	}
 }
