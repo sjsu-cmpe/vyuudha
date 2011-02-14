31ee597a7c6ffcb8bd126d3950f45d7ccfc0dab3
Utkarsh Sengar <utkarsh2012@gmail.com>
2011-02-14 15:20:17 -0800

    main moved to InitDDS from DDSServer

--- src/main/java/com/dds/init/InitDDS.java
+++ src/main/java/com/dds/init/InitDDS.java
@@ -1,5 +1,21 @@
 package com.dds.init;
 
+import java.io.IOException;
+
+import com.dds.server.nio.DDSServer;
+import com.dds.server.nio.DDSWorker;
+
 public class InitDDS {
+	
+	public static void main(String[] args) {
+		try {
+			DDSWorker worker = new DDSWorker();
+			new Thread(worker).start();
+			new Thread(new DDSServer(null, 9090, worker)).start();
+			System.out.println("Vyuudha Server Started...");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
