8b91eea7f5ffba43c429d38d3002afe5bdcfe0b7
Ravi Dontharaju <ravi.dontharaju@gmail.com>
2011-02-23 21:13:14 -0800

    - Code in place to read Server Type to initialize from a config file. - Using the beauty of interfaces to initialize the server object at runtime.

--- src/main/java/com/dds/init/InitDDS.java
+++ src/main/java/com/dds/init/InitDDS.java
@@ -1,18 +1,40 @@
 package com.dds.init;
 
-import com.dds.cluster.ClusterCommunication;
+import com.dds.interfaces.server.ServerInterface;
+import com.dds.server.bio.DDSServerBIO;
 import com.dds.server.nio.DDSServerNIO;
 
 public class InitDDS {
+	
+	String serverType;
+	
+	InitDDS() {
+		setConfiguration();
+	}
+	
+	/**
+	 * This function should read the Server Type to
+	 * initialize from a configuration file
+	 */
+	private void setConfiguration() {
+		serverType = "NIO";
+	}
 
 	public static void main(String[] args) {
+	
+		InitDDS initDDS = new InitDDS();
+		ServerInterface ddsIO;
 		
-		DDSServerNIO ddsNIO = new DDSServerNIO();
-		ddsNIO.start(null, 9090);
-		System.out.println("Vyuudha NIO Server Started...");
+		if (initDDS.serverType.equals("NIO")) {
+			ddsIO = new DDSServerNIO();	
+		} else  {  
+			ddsIO = new DDSServerBIO();
+		}
+		ddsIO.start(null, 9090);
+		System.out.println("Vyuudha " + initDDS.serverType + " Server Started...");
 		
 		// Starting JGroups channel
-		new ClusterCommunication().startCommunication();
-		System.out.println("JGroups channel established...");
+		//new ClusterCommunication().startCommunication();
+		//System.out.println("JGroups channel established...");
 	}
 }
