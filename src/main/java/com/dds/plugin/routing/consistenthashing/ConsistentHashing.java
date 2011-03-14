2a75266ee03a73b1d34d6c4d690ae4013f748978
Utkarsh Sengar <utkarsh2012@gmail.com>
2011-03-13 18:55:49 -0700

    Removed generics from ConsistentHashing.java to Node type

--- src/main/java/com/dds/plugin/routing/consistenthashing/ConsistentHashing.java
+++ src/main/java/com/dds/plugin/routing/consistenthashing/ConsistentHashing.java
@@ -3,46 +3,47 @@ package com.dds.plugin.routing.consistenthashing;
 import java.util.Collection;
 import java.util.SortedMap;
 import java.util.TreeMap;
-import com.dds.interfaces.hashing.*;
-import com.dds.utils.*;
+import com.dds.interfaces.hashing.HashingInterface;
+import com.dds.utils.Helper;
+import com.dds.cluster.Node;
 
-public class ConsistentHashing<T> {
+public class ConsistentHashing {
 
 	private final HashingInterface hashFunction;
 	private final int numberOfReplicas;
-	private final SortedMap<Integer, T> circle = new TreeMap<Integer, T>();
+	private final SortedMap<Integer, Node> circle = new TreeMap<Integer, Node>();
 
 	public ConsistentHashing(
 			HashingInterface hashFunction, 
 			int numberOfReplicas, 
-			Collection<T> nodes) {
+			Collection<Node> nodes) {
 		this.hashFunction = hashFunction;
 		this.numberOfReplicas = numberOfReplicas;
 
-		for (T node : nodes) {
+		for (Node node : nodes) {
 			add(node);
 		}
 	}
 
-	public void add(T node) {
+	public void add(Node node) {
 		for (int i = 0; i < numberOfReplicas; i++) {
 			circle.put(hashFunction.hash((node.toString() + i).getBytes()), node);
 		}
 	}
 
-	public void remove(T node) {
+	public void remove(Node node) {
 		for (int i = 0; i < numberOfReplicas; i++) {
 			circle.remove(hashFunction.hash((node.toString() + i).getBytes()));
 		}
 	}
 
-	public T get(Object key) {
+	public Node get(Object key) {
 		if (circle.isEmpty()) {
 			return null;
 		}
 		int hash = hashFunction.hash(Helper.getBytes(key));
 		if (!circle.containsKey(hash)) {
-			SortedMap<Integer, T> tailMap = circle.tailMap(hash);
+			SortedMap<Integer, Node> tailMap = circle.tailMap(hash);
 			hash = tailMap.isEmpty() ? circle.firstKey() : tailMap.firstKey();
 		}
 		return circle.get(hash);
