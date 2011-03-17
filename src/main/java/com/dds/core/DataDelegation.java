b4af41cc3d99abc3ea620c9a875a0461be2ea4b5
Utkarsh Sengar <utkarsh2012@gmail.com>
2011-03-16 21:01:56 -0700

    Added com.dds.core

---
+++ src/main/java/com/dds/core/DataDelegation.java
@@ -0,0 +1,10 @@
+package com.dds.core;
+
+/**
+ * @author utkarsh
+ *
+ *Receive a local persist or a replicate to other node request
+ */
+public class DataDelegation {
+
+}
---
+++ src/main/java/com/dds/core/RequestHandler.java
@@ -0,0 +1,11 @@
+package com.dds.core;
+
+
+/**
+ * @author utkarsh
+ *
+ * This will receive request from NIO/BIO
+ */
+public class RequestHandler {
+
+}
