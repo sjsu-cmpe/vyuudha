b64803304099b0e629526e5afdd31cb24ffc6549
Utkarsh Sengar <utkarsh2012@gmail.com>
2011-03-19 22:28:40 -0700

    Added MembershipInterface.

---
+++ src/main/java/com/dds/interfaces/MembershipInterface.java
@@ -0,0 +1,8 @@
+package com.dds.interfaces;
+
+public interface MembershipInterface {
+	public void sendNodeList();
+	public void receiveNodeList();
+	public void getLocalNodeList();
+	public void setLocalNodeList();
+}
--- src/main/java/com/dds/plugin/membership/gossip/GossipProtocol.java
+++ src/main/java/com/dds/plugin/membership/gossip/GossipProtocol.java
@@ -1,5 +1,31 @@
 package com.dds.plugin.membership.gossip;
 
-public class GossipProtocol {
+import com.dds.interfaces.MembershipInterface;
+
+public class GossipProtocol implements MembershipInterface{
+
+	@Override
+	public void sendNodeList() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void receiveNodeList() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getLocalNodeList() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setLocalNodeList() {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
