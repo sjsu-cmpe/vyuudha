48d40e298c37ccbe2bbeaeb23fff6a63e372cb7b
Ravi Dontharaju <ravi.dontharaju@gmail.com>
2011-03-23 00:52:49 -0700

    PluginEnum : Contains an Enum list of all plugins
    -Ravi

---
+++ src/main/java/com/dds/utils/PluginEnum.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.dds.utils;
+
+/**
+ * @author ravid
+ *
+ */
+public enum PluginEnum {
+	API ("API"), 
+	HASHING ("HASHING"),
+	MEMBERSHIP ("MEMBERSHIP"),
+	ROUTING ("ROUTING"),
+	SERVER ("SERVER");
+	
+	String value;
+	private PluginEnum(String str) {
+		this.value = str;
+	}
+}
