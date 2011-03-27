c39501d5db8c7497e7216bb55791a4a8c4128d49
Utkarsh Sengar <utkarsh2012@gmail.com>
2011-03-26 23:46:00 -0700

    Updated startup script. It works!

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,1 +1,14 @@
-java -Dfile.encoding=MacRoman -classpath $VYUUDHA_HOME/target/classes/
+
+#Add dependencies to classpath from target/lib
+for file in $VYUUDHA_HOME/target/lib/*.jar;
+do
+  CLASSPATH=$CLASSPATH:$file
+done
+
+#Add Vyuudha jar files to classpath from target/
+for file in $VYUUDHA_HOME/target/*.jar;
+do
+  CLASSPATH=$CLASSPATH:$file
+done
+
+java -Dlog4j.configuration=src/main/java/log4j.properties -cp $CLASSPATH com.dds.init.InitDDS
